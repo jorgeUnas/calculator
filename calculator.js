c5b7f8a34041b38f17b39658613b79bd9d535eeb
EC2 Default User <ec2-user@ip-172-31-20-212.eu-north-1.compute.internal>
2023-12-15 17:32:38 +0000

    Creating the logic behind the calculator

---
+++ calculator.js
@@ -0,0 +1,74 @@
+// Basic math operations
+
+function sum(num1, num2){
+    return parseFloat(num1) + parseFloat(num2);
+}
+function substr(num1, num2){
+    return parseFloat(num1) - parseFloat(num2);
+}
+function multiply(num1, num2){
+    return parseFloat(num1) * parseFloat(num2);
+}
+function divide(num1, num2){
+    return parseFloat(num1) / parseFloat(num2);
+}
+
+// ac button
+function allClear(){
+    const result = document.querySelector('#result');
+     result.value = 0;
+     num1 = '';
+}
+/* input the numbers 
+function input(n){
+    display(n)
+}
+*/
+
+//operands and operator
+let num1 = '';
+let num2 = '';
+let operator = '';
+let inputValue = '';
+
+
+function operate(operator){
+    
+   switch (operator) {
+       case '+':
+           const result = document.querySelector('#result');
+            if(num2 == ''){
+                num2 = num1
+                result.value = num2;
+                num1 = '';
+            }else{
+                 result.value = sum(num1, num2);
+                 num2 = sum(num1, num2);
+                 
+            }
+            
+           
+       case '-':
+           return substr(num1, num2);
+        case '*':
+           return multiply(num1, num2);
+        case '/':
+           return divide(num1, num2);
+   }
+} 
+
+// Display function
+ function input(n){
+     const result = document.querySelector('#result');
+     if(result.value == 0){
+          result.value = n;
+     }else{
+         result.value += n;
+     }
+    
+     num1 += parseFloat(n);
+    //console.log(inputValue);
+ }
+
+
+// ronded by idiots, mindset, la iliada, the power laws
