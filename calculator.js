bc564a7ecdde34100604725021bc14b5f492ebb9
EC2 Default User <ec2-user@ip-172-31-20-212.eu-north-1.compute.internal>
2023-12-21 17:10:09 +0000

    All operations working except the zero based like 0 - 6

--- calculator.js
+++ calculator.js
@@ -13,21 +13,41 @@ function divide(num1, num2){
     return parseFloat(num2) / parseFloat(num1);
 }
 
+function square(){
+    let result = document.querySelector('#result');
+    result.value = result.value ** 2;
+    num2 = result.value;
+    num1 = 0;
+}
+
+function percent(){
+    let result = document.querySelector('#result');
+    result.value = result.value / 100;
+    num2 = result.value;
+    num1 = 0;
+}
+
+function plusminus(){
+    let result = document.querySelector('#result');
+    result.value = -1 * result.value
+    num2 = result.value;
+}
+
 // ac button
 function allClear(){
     const result = document.querySelector('#result');
      result.value = 0;
-     num1 = '';
-     num2 = '';
-     newOp == false;
+     num1 = 0;
+     num2 = 0;
+     newOp = false;
     //resultValue = '';
 }
 
 
 //operands and operator
 let newOp = false;
-let num1 = '';
-let num2 = '';
+let num1 = 0;
+let num2 = 0;
 let resultValue = '';
 let operator = '';
 
@@ -46,42 +66,25 @@ function operation(){
     let result = document.querySelector('#result');
    switch (operator) {
        case '+':
-           
-           if(num2 == ''){
-              num2 = num1;
-              resultValue = `${num2} +`;
-              newOp = false;
-               
-               console.log('num1:'+ num1);
-               console.log('num1:'+ typeof num1);
-               console.log('num2:'+ num2);
-               console.log('num2:'+ typeof num2);
-               console.log(operator);
-               result.value = num2;
-           }else{
-               
+                if(num2 == 0 && num1 != 0){
+                    num2 = num1;
+                    newOp = false;
+                }else{
                num2 = sum(num1, num2);
                result.value = num2;
-               num1 = num2;
                newOp = false;
                 console.log('num1:'+ num1);
                console.log('num1:'+ typeof num1);
                console.log('num2:'+ num2);
                console.log('num2:'+ typeof num2);
                console.log(operator);
-           }
+                }
              break;
        case '-':
-           if(num2 == ''){
-               num2 = num1;
-               result.value = num2;
-               newOp = false;
-               console.log('num1:'+ num1);
-               console.log('num1:'+ typeof num1);
-               console.log('num2:'+ num2);
-               console.log('num2:'+ typeof num2);
-               console.log(operator);
-           }else{
+                if(num2 == 0 && num1 != 0){
+                    num2 = num1;
+                    newOp = false;
+                }else{
                num2 = substr(num1, num2);
                 result.value = num2;
                 newOp = false;
@@ -90,66 +93,55 @@ function operation(){
                console.log('num2:'+ num2);
                console.log('num2:'+ typeof num2);
                console.log(operator);
-           }
+                }
            break;
         case '*':
-           if(num2 == ''){
-               num2 = num1;
-               newOp = false;
-               console.log('num1:'+ num1);
-               console.log('num2:'+ num2);
-               console.log(operator);
-               result.value = `${num2}`;
-           }else{
-        
+                if(num2 == 0 && num1 != 0){
+                    num2 = num1;
+                    newOp = false;
+                }else{
                num2 = multiply(num1, num2);
                newOp = false;
                console.log('num1:'+ num1);
                console.log('num2:'+ num2);
                console.log(operator);
-                result.value = `${num2}`;
-           }
+                result.value = num2;
+                }
            break;
         case '/':
-           if(num2 == ''){
-               num2 = num1;
-               newOp = false;
-               result.value = num2;
-           }else{
+            if(num2 == 0 && num1 != 0){
+                    num2 = num1;
+                    newOp = false;
+                }else{
                num2 = divide(num1, num2);
                newOp = false;
                 result.value = num2;
-                
-               
-               
-           }
+                }
            break;
         
    }
 
 } 
 
-
 // Display function
  function input(n){
      let result = document.querySelector('#result');
-     if(newOp == false ){
+     
+     if(newOp == false ){  // This is like a swich replace num1
           result.value = n;
-          
      }else{
-         
          result.value += n;
-         
      }
-     num1 = result.value;
-     //num1 = parseFloat(n);
+     
+     num1 = parseFloat(result.value);
+     
      newOp = true;
      
-     console.log(num1);
-     console.log(typeof num1);
-    
+     console.log('num1:'+ num1);
+               console.log('num1:'+ typeof num1);
+               console.log('num2:'+ num2);
+               console.log('num2:'+ typeof num2);
  }
- 
  
  
  function equals(){
@@ -160,18 +152,10 @@ function operation(){
      } else if(operator == '*' || operator == '/'){
          num1 = 1;
      }
-     
-     /*const result = document.querySelector('#result');
-     resultValue = sum(num1, num2);
-     result.value = resultValue;
-     num2 = resultValue;
-     num1 = 0;
-     
-     */
-    /*let resultante = document.querySelector('#resultante');
-    resultante.textContent = resultValue;
-    */
+     // console.log('num1:'+ num1);
+     // console.log('num2:'+ num2);
  }
 
 
 // ronded by idiots, mindset, la iliada, the power laws
+
